f0623671f6e05cc9ebe65e91d9899961d5807d58
Samaah <skhan330@illinois.edu>
2020-02-18 20:57:47 -0600

    set up cards for player in StrategyB

--- src/test/java/student/crazyeights/StrategyBTest.java
+++ src/test/java/student/crazyeights/StrategyBTest.java
@@ -1,4 +1,25 @@
 package student.crazyeights;
 
+import static student.crazyeights.Card.Rank;
+import static student.crazyeights.Card.Suit;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StrategyBTest {
+    StrategyB strategyB;
+    @Before
+    public void setUp() {
+        strategyB = new StrategyB();
+        strategyB.playerCards.add(new Card(Suit.CLUBS, Rank.EIGHT));
+        strategyB.playerCards.add(new Card(Suit.DIAMONDS, Rank.ACE));
+        strategyB.playerCards.add(new Card(Suit.SPADES, Rank.KING));
+        strategyB.playerCards.add(new Card(Suit.HEARTS, Rank.NINE));
+        strategyB.playerCards.add(new Card(Suit.DIAMONDS, Rank.TWO));
+    }
+
 }
